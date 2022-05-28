5ac64deb97fce8606678f356d740fca7c2792a32
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-05-29 00:14:10 +0900

    router 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router } from "react-router-dom";
+import Home from "./pages";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <Home />
+    </Router>
   );
 }
 
