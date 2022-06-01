0fd81404086ecf49c847132542ee99d27fab9291
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-01 12:40:37 +0900

    Services Footer 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,8 @@ import {
   homeObjTree,
   homeObjTwo,
 } from "../components/InfoSection/Data";
+import Services from "../components/Services";
+import Footer from "../components/Footer";
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,7 +25,9 @@ const Home = () => {
       <HeroSection />
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
+      <Services />
       <InfoSection {...homeObjTree} />
+      <Footer />
     </>
   );
 };
