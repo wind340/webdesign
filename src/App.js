0d73da54e2bf2614b5e13724d56c5769a3d8715c
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-02 22:52:13 +0900

    register 연결

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages";
 import SigninPage from "./pages/signin";
+import RegisterPage from "./components/Register";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} exact />
         <Route path="/signin" element={<SigninPage />} exact />
+        <Route path="/register" element={<RegisterPage />} exact />
       </Routes>
     </Router>
   );
