bcae9bdeae1f579796e4c58b40921f1b0410a874
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-02 22:54:17 +0900

    register page

---
+++ src/pages/register.js
@@ -0,0 +1,14 @@
+import Register from "components/Register";
+import React from "react";
+import ScrollToTop from "../components/ScrollToTop";
+
+const RegisterPage = () => {
+  return (
+    <>
+      <ScrollToTop />
+      <Register />
+    </>
+  );
+};
+
+export default RegisterPage;
