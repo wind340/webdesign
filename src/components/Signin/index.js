1d3ecc87082271c1b438ec780dce2dbb09fc3264
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-02 22:47:51 +0900

    Register page set

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,4 +1,6 @@
-import React from "react";
+import { authService } from "fbase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import {
   FormWrap,
   FormContent,
@@ -6,13 +8,42 @@ import {
   FormInput,
   FormButton,
   FormLabel,
-  Text,
   Container,
   Icon,
   Form,
+  FormLink,
+  Text,
 } from "./SigninElements";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const onChange = event => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = async event => {
+    event.preventDefault();
+    try {
+      const data = await signInWithEmailAndPassword(
+        authService,
+        email,
+        password
+      );
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
     <>
       <Container>
@@ -22,11 +53,28 @@ const SignIn = () => {
             <Form action="#">
               <FormH1> Sign in to your account</FormH1>
               <FormLabel htmlFor="for">Email</FormLabel>
-              <FormInput type="email" required />
+              <FormInput
+                name="email"
+                type="email"
+                required
+                value={email}
+                onChange={onChange}
+              />
               <FormLabel htmlFor="for">Password</FormLabel>
-              <FormInput type="password" required />
+              <FormInput
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChange}
+              />
               <FormButton type="submit">Continue</FormButton>
-              <Text>Forgot password</Text>
+              <FormLink to="/Register">
+                Don't have an Account?
+                <br /> Creact an account here
+              </FormLink>
+              <Text>{error}</Text>
             </Form>
           </FormContent>
         </FormWrap>
