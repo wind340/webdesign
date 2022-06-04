97181be0ec3493ec3826d6b86c80f072a2181009
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-06-04 23:49:17 +0900

    Update index.js

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Container,
   BoardWrapper,
+  BoardForm,
+  BoardFromWrapper,
   BoardListWrapper,
   BoardList,
   BoardTitle,
   BoardWriter,
   BoardContents,
 } from "./BoardElements";
+
 const Board = () => {
+  const [writer, setWriter] = useState("");
+  const [title, setTitle] = useState("");
+  const [contents, setContents] = useState("");
+
+  const onChange = e => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === writer) {
+      setWriter(value);
+    } else if (name === title) {
+      setTitle(value);
+    } else if (name === contents) {
+      setContents(value);
+    }
+  };
+
+  const onSubmit = () => {};
+
   return (
     <Container>
       <BoardWrapper>
+        <BoardFromWrapper>
+          <BoardForm onSubmit={onSubmit}>
+            <BoardTitle name="title" type="text" value={title} />
+            <BoardWriter name="writer" type="text" value={writer} />
+            <BoardContents name="contents" type="text" value={contents} />
+          </BoardForm>
+        </BoardFromWrapper>
         <BoardListWrapper>
-          <BoardList>
-            <BoardTitle>{}</BoardTitle>
-            <BoardWriter>{}</BoardWriter>
-            <BoardContents>{}</BoardContents>
-          </BoardList>
+          <BoardList></BoardList>
         </BoardListWrapper>
       </BoardWrapper>
     </Container>
